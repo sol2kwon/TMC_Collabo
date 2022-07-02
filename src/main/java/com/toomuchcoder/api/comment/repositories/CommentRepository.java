74febd8be9b0118c32ee3f9a7b3f0bcb86512865
sol2kwon <amorfatisole@gmail.com>
2022-07-02 20:52:44 +0900

    20220702

--- src/main/java/com/toomuchcoder/api/comment/repositories/CommentRepository.java
+++ src/main/java/com/toomuchcoder/api/comment/repositories/CommentRepository.java
@@ -22,6 +22,7 @@ interface CommentCustomeReposittory{
     @Query(value="update Comment c set c.comment = :comment  where c.commentid = :postid",
             nativeQuery = true)
     //만약 댓글이 수정된다면 수정 date 자동등록 어떻게 할지?
+    // 테이블 그대로 불러오면 안되는거 아님??
     int update(@Param("comment") String comment);
 }
 @Repository
